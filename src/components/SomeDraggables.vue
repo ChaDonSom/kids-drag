a3e03143d729f6bef92b6404db3f148f91bf65b9
Chase Spin 713 <chase93@live.com>
2022-01-22 16:32:36 -0500

    add draggable

---
+++ src/components/SomeDraggables.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import draggable from 'vuedraggable'
+
+const draggables = ref([
+  {
+    label: 'Hi'
+  }
+])
+</script>
+
+<template>
+  <draggable
+      :list="draggables"
+  >
+    <template #item="{ element }">
+      <div>{{ element.label }}</div>
+    </template>
+  </draggable>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+import SomeDraggables from '@/components/SomeDraggables.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <SomeDraggables />
   </main>
 </template>
