4b2358e921e68cd0603dcb13ccb8356bce5ad8bd
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-01-24 20:47:44 -0500

    deploy

---
+++ assets/AboutView.3f298a3a.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.d1c149a6.js";import{o,f as t,g as s}from"./vendor.c155ea93.js";const _={},a={class:"about"},c=s("h1",null,"This is an about page",-1),n=[c];function r(i,d){return o(),t("div",a,n)}var f=e(_,[["render",r]]);export{f as default};
--- assets/AboutView.8322c52e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index.5c392ef4.js";import{o,f as t,g as s}from"./vendor.c155ea93.js";const _={},a={class:"about"},c=s("h1",null,"This is an about page",-1),n=[c];function r(i,d){return o(),t("div",a,n)}var l=e(_,[["render",r]]);export{l as default};
