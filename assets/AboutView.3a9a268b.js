770cc5bc66e1b4398e9a9a39262d97073cd4bb4b
Chase Drift Ubuntu <chadonsom@gmail.com>
2022-01-29 14:09:33 -0500

    deploy

---
+++ assets/AboutView.3a9a268b.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.a47fc8dc.js";import{o,f as t,g as s}from"./vendor.b7e06740.js";const _={},a={class:"about"},c=s("h1",null,"This is an about page",-1),n=[c];function r(i,d){return o(),t("div",a,n)}var l=e(_,[["render",r]]);export{l as default};
